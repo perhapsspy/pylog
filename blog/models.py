e2ac049c50b443b4a40080785e090e3a81ae6661
정경업 <py@smartstudy.co.kr>
2015-02-04 20:37:02 +0900

    모델 정의

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = u'분류'
+        ordering = ['name']
+
+    name = models.CharField(verbose_name=u'이름', max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = u'글'
+        ordering = ['created']
+
+    category = models.ForeignKey(Category, verbose_name=u'분류', null=True, blank=True)
+    title = models.CharField(verbose_name=u'제목', max_length=256)
+    content = models.TextField(u'내용', blank=True, default='')
+    created = models.DateTimeField(auto_now_add=True, verbose_name=u'생성일')
+
+    def __unicode__(self):
+        return self.title
