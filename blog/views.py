148b4de0f7d166933bf3e9d327d4d7195a40cbaa
정경업 <py@smartstudy.co.kr>
2015-02-04 21:22:14 +0900

    디테일 뷰 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
@@ -7,4 +7,11 @@ def home(request):
     template_name = 'home.html'
     posts = Post.objects.all()
     context_data = {'object_list': posts}
+    return render(request, template_name, context_data)
+
+
+def detail(request, pk):
+    template_name = 'detail.html'
+    post = get_object_or_404(Post, id=pk)
+    context_data = {'object': post}
     return render(request, template_name, context_data)
--- pylog/urls.py
+++ pylog/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'blog.views.home', name='home'),
+    url(r'^(?P<pk>\d+)/$', 'blog.views.detail', name='detail'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
