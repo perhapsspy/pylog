0c00fb190b48dcbe7a916666ef3d77a3c1a727b0
정경업 <py@smartstudy.co.kr>
2015-02-04 21:13:35 +0900

    홈에서 글 목록 보여주기

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from blog.models import Post
 
 
 def home(request):
     template_name = 'home.html'
-    return render(request, template_name)
+    posts = Post.objects.all()
+    context_data = {'object_list': posts}
+    return render(request, template_name, context_data)
