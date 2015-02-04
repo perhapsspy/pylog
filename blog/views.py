06c31a37649d8cc52a2199886b67be3acf49f885
정경업 <py@smartstudy.co.kr>
2015-02-04 20:56:03 +0900

    홈 뷰 생성

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('hi')
