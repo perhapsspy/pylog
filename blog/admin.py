0e4f4a6bfe4a27010fac67bec10215a7fa11ee43
정경업 <py@smartstudy.co.kr>
2015-02-04 20:38:05 +0900

    admin 정의

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Category, Post
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_editable = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created']
+    list_filter = ['category']
+    list_display_links = ['id', 'title']
+    search_fields = ['title', 'content', ]
+    ordering = ['-created']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
