2f38bbd60055029022abff4c4cd1e3a0d9028095
정경업 <py@smartstudy.co.kr>
2015-02-04 21:45:19 +0900

    목록에서 분류 표시

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import ListView, DetailView, CreateView
 
-from blog.models import Post
+from blog.models import Post, Category
 
 
 class PostList(ListView):
     model = Post
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        categories = Category.objects.all()
+        context['categories'] = categories
+        return context
 
 
 class PostDetail(DetailView):
