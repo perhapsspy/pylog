1b487115c9f6602a7a65464096ef11512f3d520f
woojing <woojing@fast.pe.kr>
2015-02-04 22:32:49 +0900

    기본 모델, 리스팅 테스트 케이스 추가

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from blog.models import Post
 
-# Create your tests here.
+
+class TestPostModel(TestCase):
+    def _create_post(self):
+        Post.objects.create(title=u'테스트',
+                            content=u'내용')
+
+    def test_create_post_model(self):
+        self._create_post()
+        posts = Post.objects.all()
+        self.assertEqual(len(posts), 1)
+        post = posts[0]
+        self.assertEqual(post.title, u'테스트')
+        self.assertEqual(post.content, u'내용')
+
+    def test_basic_listing(self):
+        self._create_post()
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['object_list']), 1)
+        object_list = response.context['object_list']
+        post = object_list[0]
+        self.assertEqual(post.title, u'테스트')
